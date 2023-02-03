da8707272e4e5b8a42acdcb3e1e1ef34cd1e9d6a
zazaz <zazaza993@mail.ru>
2023-02-03 11:46:19 +0700

    Initial commit

---
+++ authorization.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Authorization</title>
+</head>
+<body>
+<form action="" method="">
+    <label for="">Логин</label>
+    <input type="text" placeholder="введите логин">
+    <label for="">Пароль</label>
+    <input type="password" placeholder="введите пароль">
+    <button>Войти</button>
+    <a href="#"></a>
+    <p>
+        У вас нет акка? - <a href="#">зарегистрируйтесь!</a>
+    </p>
+</form>
+</body>
+</html>
